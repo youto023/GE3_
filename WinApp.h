0fa102d03d8ce5e0abfff852d6a407e92f86381d
youto023 <yuuto9036@gmail.com>
2024-11-11 16:12:37 +0900

    03_01完成

---
+++ WinApp.h
@@ -0,0 +1,10 @@
+#pragma once
+#include"Windows.h"
+class WinApp
+{
+	//初期化
+public://メンバ変数
+	void Initialiize();
+	//更新
+	void Update();
+};
--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@
 #include<dxcapi.h>
 #include"externals/imgui/imgui.h"
 #include"externals/imgui/imgui_impl_dx12.h"
-#include"externals/imgui/imgui_impl_win32.h"
+//#include"externals/imgui/imgui_impl_win32.h"
 #include"Matrix.h"
 #include"Transform.h"
 #include"externals/DirectXTex/DirectXTex.h"
@@ -55,32 +55,6 @@ struct ModelData {
 	MaterialData material;
 };
 
-
-
-//ウィンドウプロシージャ
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
-
-	if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wparam, lparam))
-	{
-		return true;
-	}
-
-
-
-	//メッセージに応じてゲーム固有の処理を行う
-	switch (msg) {
-		//ウィンドウが破棄された
-	case WM_DESTROY:
-		//osに対して、アプリの終了を伝える
-		PostQuitMessage(0);
-		return 0;
-
-	}
-
-	//標準のメッセージ処理を行う
-	return DefWindowProc(hwnd, msg, wparam, lparam);
-
-}
 
 std::wstring ConvertString(const std::string& str) {
 	if (str.empty()) {
@@ -476,80 +450,7 @@ ModelData LoadObjFile(const std::string& directoryPath, const std::string& filen
 //Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {//main関数
 
-	CoInitializeEx(0, COINIT_MULTITHREADED);
 
-
-	const int32_t kCLientWidth = 1280;
-	const int32_t kCLientHeight = 720;
-
-	RECT wrc = { 0,0,kCLientWidth,kCLientHeight };
-
-	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
-
-
-
-	//出力ウィンドウへの文字出力
-	OutputDebugStringA("Hello,DirectX!\n");
-
-	WNDCLASS wc{};
-	//ウィンドウプロシージャ
-	wc.lpfnWndProc = WindowProc;
-
-	//ウィンドウクラス名
-	wc.lpszClassName = L"CG2WindowClass";
-
-	//インスタンスハンドル
-	wc.hInstance = GetModuleHandle(nullptr);
-
-	//カーソル
-	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
-
-	//ウィンドウクラスを登録する
-	RegisterClass(&wc);
-
-	//ウィンドウの生成
-	HWND hwnd = CreateWindow(
-		wc.lpszClassName,
-		L"CG2",
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		wrc.right - wrc.left,
-		wrc.bottom - wrc.top,
-		nullptr,
-		nullptr,
-		wc.hInstance,
-		nullptr
-	);
-
-	//#ifdef DEBUG
-	//	ID3D12Debug1* debugController = nullptr;
-	//	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)))) {
-	//		//デバックレイヤーを有効化する
-	//		debugController->EnableDebugLayer();
-	//		//さらにGPU側でもチェックを行うようにする
-	//		debugController->SetEnableGPUBasedValidation(TRUE);
-	//	}
-	//#endif // DEBUG
-
-#ifdef _DEBUG
-	ID3D12Debug1* debugController = nullptr;
-	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)))) {
-		//デバックレイヤーを有効化する
-		debugController->EnableDebugLayer();
-		//さらにGPU側でもチェックを行うようにする
-		debugController->SetEnableGPUBasedValidation(TRUE);
-	}
-
-#endif
-
-
-
-
-
-
-
-	ShowWindow(hwnd, SW_SHOW);
 
 	//ポインタ
 	Input* input = nullptr;
