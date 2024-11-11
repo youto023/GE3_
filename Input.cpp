7ba9d30fe8c36ad2d0fdb0979c232a2339e7e4b5
youto023 <yuuto9036@gmail.com>
2024-11-11 14:13:10 +0900

    02_01完成

---
+++ Input.cpp
@@ -0,0 +1,39 @@
+#define DIRECTINPUT_VERSION  0x0800//DirectInputのバージョン指定
+#include "Input.h"
+#include <cassert>
+#include<wrl.h>
+#include<dinput.h>
+#pragma comment(lib,"dinput8.lib")
+#pragma comment(lib,"dxguid.lib")
+using namespace Microsoft::WRL;
+void Input::Initialize(HINSTANCE hInstance, HWND hwnd)
+{
+	HRESULT result;
+
+	//DirectInputの初期化(一度だけ行う処理)
+	IDirectInput8* directInput = nullptr;
+	result = DirectInput8Create(
+		hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8,
+		(void**)&directInput, nullptr);
+	assert(SUCCEEDED(result));
+
+	//キーボードデバイスの生成
+	IDirectInputDevice8* keyboard = nullptr;
+	result = directInput->CreateDevice(GUID_SysKeyboard, &keyboard, NULL);
+	assert(SUCCEEDED(result));
+
+	//入力データ形式のセット
+	result = keyboard->SetDataFormat(&c_dfDIKeyboard);//標準形式
+	assert(SUCCEEDED(result));
+
+	//排他制御レベルのセット
+	result = keyboard->SetCooperativeLevel(
+
+		hwnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY);
+	assert(SUCCEEDED(result));
+}
+
+void Input::Update()
+{
+
+}
